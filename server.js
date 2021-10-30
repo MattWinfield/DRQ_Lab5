28c4d84156fa6e3b719ade11ec4a16b487715a1a
unknown <matt.winfield98@gmail.com>
2021-10-30 22:01:43 +0100

    Exercise 5

--- server.js
+++ server.js
@@ -10,6 +10,29 @@ app.get('/hello/:name', (req, res) => {//Create another Node response to a HTTP 
     res.send('Hello ' + req.params.name)
 })
 
+app.get('/api/movies', (req, res) => {//Create another Node response to a HTTP Get request at the /api/movies Address of localHost, responding with JSON Data
+    const myMovies =//Create a variabe to store the Json Data
+    {
+        "movies": [
+            {
+                "Title": "Avengers: Infinity War",
+                "Year": "2018",
+                "imdbID": "tt4154756",
+                "Type": "movie",
+                "Poster": "https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_SX300.jpg"
+            },
+            {
+                "Title": "Captain America: Civil War",
+                "Year": "2016",
+                "imdbID": "tt3498820",
+                "Type": "movie",
+                "Poster": "https://m.media-amazon.com/images/M/MV5BMjQ0MTgyNjAxMV5BMl5BanBnXkFtZTgwNjUzMDkyODE@._V1_SX300.jpg"
+            }
+        ]
+    }
+    res.json({ movies: myMovies })//Respond with Json Data
+})
+
 app.listen(port, () => {//Create a Node HTTP Server and Specify the Port to listen with the 'port' Variable
     console.log(`Example app listening at http://localhost:${port}`)
 })
