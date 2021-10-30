5e7d21d20ce131602aeb74f1e1e475109d101fc0
unknown <matt.winfield98@gmail.com>
2021-10-30 22:32:22 +0100

    Exercise 8

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 const express = require('express') //Creates a variable to Include Express with the node REQUIRE Function
 const pat = require('path') //Creates a variable to Include Path with the node REQUIRE Function
+const bodyParser = require('body-parser') //Creates a variable to Include Body-Parser with the node REQUIRE Function
 const app = express() //Create an Express application and store as a Varaible
 const port = 3000 //Define a Port variable
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
 
 app.get('/', (req, res) => {//Create a Node response to a HTTP Get request at the root Address of localHost, responding with a message
     res.send('Welcome to Data Representation & Querying!')
@@ -38,8 +45,12 @@ app.get('/test', (req, res) => {//Create another Node response to a HTTP Get req
     res.sendFile(__dirname + '/index.html')//respond with the sendFile function
 })
 
-app.get('/name', (req, res) => {//Create another Node response to a HTTP Get request at the /name Address of localHost, responding with an HTML Page
-    res.send('Hello ' + req.query.fname + ' ' + req.query.lname)//respond with the sendFile function
+app.get('/name', (req, res) => {//Create another Node response to a HTTP Get request at the /name Address of localHost, responding with GET Method Data
+    res.send('Hello ' + req.query.fname + ' ' + req.query.lname)//respond with the GET function Data
+})
+
+app.post('/name', (req, res) => {//Create another Node response to a HTTP Post request at the /name Address of localHost, responding with POST Method Data
+    res.send('Hello ' + req.body.fname + ' ' + req.body.lname)//respond with the POST function Data
 })
 
 app.listen(port, () => {//Create a Node HTTP Server and Specify the Port to listen with the 'port' Variable
